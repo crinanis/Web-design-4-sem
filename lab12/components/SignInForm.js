07d7be4639da8e3d310fe81f0fce98b9f6c876df
ksesha <72476438+crinanis@users.noreply.github.com>
2022-05-26 22:36:09 +0300

    Add files via upload

---
+++ lab12/components/SignInForm.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {store} from "../index"
+import "../index.css"
+
+export class SignInForm extends React.Component {
+    state={
+        password: null,
+        login: null
+    }
+
+    changeHandler(e, target) {
+        this.setState({[target]: e.target.value})
+    }
+
+    handleClick = (e) => {
+        e.preventDefault();
+        if(this.state.login === store.getState().login && this.state.password === store.getState().password)
+        this.props.in(true)
+    }
+
+    render() {
+        return(
+            <div className="sign_in">
+                <p>login/email</p>
+                <input type="text" onChange={(e) => this.changeHandler(e, "login")} />
+                <p>password</p>
+                <input type="password" onChange={(e) => this.changeHandler(e, "password")} />
+                <button className="send" onClick={this.handleClick}>send</button>
+            </div>
+        )
+    }
+}
